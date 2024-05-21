bfeaac6bfc931cbb087aa5ea60930b028b9db46b
hana <744325576@qq.com>
2024-05-21 19:30:02 +0800

    重大更新！添加了完成任务时播放的音效，可以自行通过替换Sound.wav的方式自定义。 以及其他的小修复：优化界面，修复了非音频文件拖拽时的报错，增加了没有控制台的exe版本

--- main.py
+++ main.py
@@ -5,33 +5,35 @@ import threading
 from tkinterdnd2 import DND_FILES, TkinterDnD
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
-
+from tkinter import messagebox
+import json
 AudioSegment.converter = "ffmpeg.exe"
-
+from playsound import playsound
 
 
 def export_audio(sound, output_path):
     """将有声音频导出为一个文件"""
     detail = ""
-    if FuncMode.get() == "analysis":
-        detail = "_干音分析"
-        print(detail)
-    else:
-        detail = "_音频处理"
-        if AudioType.get() == "auto":
-            detail += "_"+AutoDetect.get()
+    if NameValue.get() == 1:
+        if FuncMode.get() == "analysis":
+            detail = "_干音分析"
+            print(detail)
         else:
-            if AudioType.get() == "mono":
-                detail += "_mono"
+            detail = "_音频处理"
+            if AudioType.get() == "auto":
+                detail += "_"+AutoDetect.get()
             else:
-                detail += "_stereo"
-        if SampleRate.get() == 48000:
-            detail += "_48000hz"
-        else:
-            detail += "_44100hz"
-        if Normalize.get():
-            detail += "_normalized"
-        print(detail)
+                if AudioType.get() == "mono":
+                    detail += "_mono"
+                else:
+                    detail += "_stereo"
+            if SampleRate.get() == 48000:
+                detail += "_48000hz"
+            else:
+                detail += "_44100hz"
+            if Normalize.get():
+                detail += "_normalized"
+            print(detail)
     #获取文件名
     output_path = output_path.replace("\\","/")
     filename = output_path.split("/")[-1]
@@ -80,24 +82,31 @@ def analysis_files(audio_paths):
     current_file_index = 0
     total_files = len(audio_paths)
     progress_var = tk.DoubleVar()
-    progress_bar = ttk.Progressbar(app, variable=progress_var, maximum=total_files)
-    progress_bar.pack(pady=10)
+    progress_bar = ttk.Progressbar(middle_frame, variable=progress_var, maximum=total_files)
+    progress_bar.grid(row=16, column=0, pady=0, sticky='n')
     for audio_path in audio_paths:
+        if is_audio_file(audio_path) == False:
+            current_file_index += 1
+            continue
         current_file_index += 1
         result_var.set(f"正在处理第 {current_file_index}/{total_files} 个文件")
         progress_var.set(current_file_index)
         app.update_idletasks()
         total_duration += analyze_audio(audio_path,threshold)
     result_var.set(f"有声音的总长度（去除静音部分）：{total_duration / 1000}秒")
+    playsound('sound.wav')
     progress_bar.destroy()
 def process_files(audio_paths):
     #将音频转换为AudioType和SampleRate对应格式
     current_file_index = 0
     total_files = len(audio_paths)
     progress_var = tk.DoubleVar()
-    progress_bar = ttk.Progressbar(app, variable=progress_var, maximum=total_files)
-    progress_bar.pack(pady=10)
+    progress_bar = ttk.Progressbar(middle_frame, variable=progress_var, maximum=total_files)
+    progress_bar.grid(row=16, column=0, pady=0, sticky='n')
     for audio_path in audio_paths:
+        if is_audio_file(audio_path) == False:
+            current_file_index += 1
+            continue
         current_file_index += 1
         result_var.set(f"正在处理第 {current_file_index}/{total_files} 个文件")
         progress_var.set(current_file_index)
@@ -131,73 +140,31 @@ def process_files(audio_paths):
         export_audio(sound, audio_path)
     progress_bar.destroy()
     result_var.set(f"处理完成")
+    playsound('sound.wav')
 def on_checkA():
 
     if AValue.get() == 1:
         FuncMode.set("analysis")
         print(FuncMode.get())
-        #显示阈值输入框
-        threshold_entry.pack()
-        threshold_label.pack()
         #将另一个checkbox设置为不选中
         PValue.set(0)
         app.update_idletasks()
-        #隐藏单声道、双声道、48000hz、44100hz等checkbox
-        chkStereo.pack_forget()
-        chkMono.pack_forget()
-        chkAuto.pack_forget()
-        chk48000.pack_forget()
-        chk44100.pack_forget()
-        chkNmlz.pack_forget()
-        chkOutputFormat.pack_forget()
     else:
         #隐藏阈值输入框
         FuncMode.set("process")
         print(FuncMode.get())
-        threshold_entry.pack_forget()
-        threshold_label.pack_forget()
         PValue.set(1)
-        #显示单声道、双声道、48000hz、44100hz等checkbox
-        chkStereo.pack()
-        chkMono.pack()
-        chkAuto.pack()
-        chk48000.pack()
-        chk44100.pack()
-        chkNmlz.pack()
-        chkOutputFormat.pack()
 
 def on_checkP():
     if PValue.get() == 1:
         FuncMode.set("process")
         print(FuncMode.get())
-        #隐藏阈值输入框
-        threshold_entry.pack_forget()
-        threshold_label.pack_forget()
         #将另一个checkbox设置为不选中
         AValue.set(0)
-        #显示单声道、双声道、48000hz、44100hz等checkbox
-        chkStereo.pack()
-        chkMono.pack()
-        chkAuto.pack()
-        chk48000.pack()
-        chk44100.pack()
-        chkNmlz.pack()
-        chkOutputFormat.pack()
     else:
         FuncMode.set("analysis")
         print(FuncMode.get())
         AValue.set(1)
-        #隐藏阈值输入框
-        threshold_entry.pack()
-        threshold_label.pack()
-        #隐藏单声道、双声道、48000hz、44100hz等checkbox
-        chkStereo.pack_forget()
-        chkMono.pack_forget()
-        chkAuto.pack_forget()
-        chk48000.pack_forget()
-        chk44100.pack_forget()
-        chkNmlz.pack_forget()
-        chkAuto.pack_forget()
 def on_checkStereo():
     if SValue.get() == 1:
         AudioType.set("stereo")
@@ -270,53 +237,151 @@ def on_checkOutputFormat():
     else:
         OutputFormat.set("mp3")
         print("mp3")
+def on_checkName():
+    if NValue.get() == 1:
+        print("添加命名后缀")
+    else:
+        print("不添加命名后缀")
 
+def is_audio_file(file_path):
+    """判断文件是否为能处理的音频文件"""
+    try:
+        AudioSegment.from_file(file_path)
+        return True
+    except:
+        return False
+def handle_file_1(event):
+    # 分析
+    global current_file_index
+    audio_paths = app.tk.splitlist(event.data)
+    FuncMode.set("analysis")
+    threading.Thread(target=analysis_files, args=(audio_paths,)).start()
+
+def handle_file_2(event):
+    # 处理
+    global current_file_index
+    audio_paths = app.tk.splitlist(event.data)
+    FuncMode.set("process")
+    threading.Thread(target=process_files, args=(audio_paths,)).start()
+
+def read_json_file():
+    """读取 JSON 文件并提取 color1 和 color2"""
+    bool = True
+    try:
+        with open("config.json", 'r') as file:
+            data = json.load(file)
+            color1 = data.get("color1", "未找到 color1")
+            color2 = data.get("color2", "未找到 color2")
+    except Exception as e:
+        bool = False
+        messagebox.showerror("错误", f"读取 JSON 文件时出错: {e}")
+    if bool:
+        return color1, color2
+    else:
+        return "lightblue", "lightgreen"
+
+# 创建主窗口
 app = TkinterDnD.Tk()
 app.title('DryVocalTimeAnalyser')
 app.iconbitmap('icon.ico')
-#默认为干音分析
+app.geometry('600x450')
+colorA = "lightblue"
+colorP = "lightgreen"
+colorA,colorP = read_json_file()
+# 设置变量
 AValue = tk.IntVar(value=1)
 PValue = tk.IntVar(value=0)
-#默认为48000hz
+NameValue = tk.IntVar(value=1)
 C48Value = tk.IntVar(value=1)
 C41Value = tk.IntVar(value=0)
-#默认为自动检测
 SValue = tk.IntVar(value=0)
 MValue = tk.IntVar(value=0)
 ATValue = tk.IntVar(value=1)
-#默认为不标准化
 NValue = tk.IntVar(value=0)
 FuncMode = tk.StringVar(value="analysis")
 SampleRate = tk.IntVar(value=48000)
 AudioType = tk.StringVar(value="auto")
 Normalize = tk.BooleanVar(value=False)
 AutoDetect = tk.StringVar(value="None")
-#导出格式
 OutputFormat = tk.StringVar(value="wav")
-#默认为干音分析
-chkA = ttk.Checkbutton(app, text='干音分析', variable=AValue,onvalue=1,offvalue=0,command=on_checkA)
-chkA.pack()
-chkP = ttk.Checkbutton(app, text='素材处理', variable=PValue,onvalue=1,offvalue=0,command=on_checkP)
-chkP.pack()
-chkStereo = ttk.Checkbutton(app, text='双声道', variable=SValue,onvalue=1,offvalue=0,command=on_checkStereo)
-chkMono = ttk.Checkbutton(app, text='单声道', variable=MValue,onvalue=1,offvalue=0,command=on_checkMono)
-chkAuto = ttk.Checkbutton(app, text='自动检测', variable=ATValue,onvalue=1,offvalue=0,command=on_checkAuto)
-chk48000 = ttk.Checkbutton(app, text='48000hz', variable=C48Value,onvalue=1,offvalue=0,command=on_check48000)
-chk44100 = ttk.Checkbutton(app, text='44100hz', variable=C41Value,onvalue=1,offvalue=0,command=on_check44100)
-chkNmlz = ttk.Checkbutton(app, text='标准化', variable=NValue,onvalue=1,offvalue=0,command=on_checkNmlz)
-chkOutputFormat = ttk.Checkbutton(app, text='导出为mp3(默认wav)', variable=OutputFormat,onvalue="mp3",offvalue="wav",command=on_checkOutputFormat)
-result_var = tk.StringVar()
-result_label = tk.Label(app, textvariable=result_var)
-result_label.pack(pady=10)
-threshold_entry = Entry(app)
-threshold_entry.pack(pady=10)
-threshold_var = tk.StringVar()
-threshold_label = tk.Label(app, textvariable=threshold_var)
-threshold_label.pack(pady=10)
-threshold_var.set("输入阈值（默认-40）")
-result_var.set("请直接拖拽音频文件到此窗口\n程序会自动处理并输出处理结果到原文件路径下\n文件名后会加上“修改”字样以示区别")
-app.drop_target_register(DND_FILES)
-app.dnd_bind('<<Drop>>', on_drop)
 
-app.geometry('400x300')
+# 使用grid布局管理器
+app.columnconfigure(0, weight=1, minsize=150)
+app.columnconfigure(1, weight=1, minsize=300)
+app.columnconfigure(2, weight=1, minsize=150)
+app.rowconfigure(0, weight=1)
+
+# 创建两个区域
+frame1 = tk.Frame(app, bg=colorA)
+frame1.grid(row=0, column=0, padx=0, pady=0, sticky='nsew')
+
+frame2 = tk.Frame(app, bg=colorP)
+frame2.grid(row=0, column=2, padx=0, pady=0, sticky='nsew')
+
+# 为两个区域设置拖拽文件功能
+frame1.drop_target_register(DND_FILES)
+frame1.dnd_bind('<<Drop>>', lambda event: handle_file_1(event))
+
+frame2.drop_target_register(DND_FILES)
+frame2.dnd_bind('<<Drop>>', lambda event: handle_file_2(event))
+
+# 在frame1中添加文字
+label1 = tk.Label(frame1, text="干音分析", bg=colorA)
+label1.pack(expand=True)
+
+# 在frame2中添加文字
+label2 = tk.Label(frame2, text="素材处理", bg=colorP)
+label2.pack(expand=True)
+
+
+middle_frame = tk.Frame(app)
+middle_frame.grid(row=0, column=1, padx=0, pady=10, sticky='ns')
+# 在中间放置其他控件
+#chkA = ttk.Checkbutton(middle_frame, text='干音分析', variable=AValue, onvalue=1, offvalue=0, command=on_checkA)
+#chkP = ttk.Checkbutton(middle_frame, text='素材处理', variable=PValue, onvalue=1, offvalue=0, command=on_checkP)
+chkN = ttk.Checkbutton(middle_frame, text='添加命名后缀', variable=NameValue, onvalue=1, offvalue=0, command=on_checkName)
+chkStereo = ttk.Checkbutton(middle_frame, text='双声道', variable=SValue, onvalue=1, offvalue=0, command=on_checkStereo)
+chkMono = ttk.Checkbutton(middle_frame, text='单声道', variable=MValue, onvalue=1, offvalue=0, command=on_checkMono)
+chkAuto = ttk.Checkbutton(middle_frame, text='自动检测', variable=ATValue, onvalue=1, offvalue=0, command=on_checkAuto)
+chk48000 = ttk.Checkbutton(middle_frame, text='48000hz', variable=C48Value, onvalue=1, offvalue=0, command=on_check48000)
+chk44100 = ttk.Checkbutton(middle_frame, text='44100hz', variable=C41Value, onvalue=1, offvalue=0, command=on_check44100)
+chkNmlz = ttk.Checkbutton(middle_frame, text='标准化', variable=NValue, onvalue=1, offvalue=0, command=on_checkNmlz)
+chkOutputFormat = ttk.Checkbutton(middle_frame, text='导出为mp3(默认wav)', variable=OutputFormat, onvalue="mp3", offvalue="wav", command=on_checkOutputFormat)
+
+#chkA.grid(row=0, column=0, sticky='n', pady=0)
+#chkP.grid(row=1, column=0, sticky='n', pady=0)
+chkN.grid(row=2, column=0, sticky='n', pady=0)
+chkStereo.grid(row=3, column=0, sticky='n', pady=0)
+chkMono.grid(row=4, column=0, sticky='n', pady=0)
+chkAuto.grid(row=5, column=0, sticky='n', pady=0)
+chk48000.grid(row=6, column=0, sticky='n', pady=0)
+chk44100.grid(row=7, column=0, sticky='n', pady=0)
+chkNmlz.grid(row=8, column=0, sticky='n', pady=0)
+chkOutputFormat.grid(row=9, column=0, sticky='n', pady=0)
+
+result_var = tk.StringVar()
+result_label = tk.Label(middle_frame, textvariable=result_var)
+result_label.grid(row=15, column=0, pady=0, sticky='n')
+
+threshold_entry = tk.Entry(middle_frame)
+threshold_entry.grid(row=12, column=0, pady=0, sticky='n')
+
+threshold_var = tk.StringVar()
+threshold_label = tk.Label(middle_frame, textvariable=threshold_var)
+threshold_label.grid(row=13, column=0, pady=0, sticky='n')
+threshold_var.set("输入阈值（默认-40）")
+
+result_var.set("这里显示处理进度和结果")
+
+split_label1 = tk.Label(middle_frame, text="--------------素材处理设定--------------", font=("Helvetica", 10, "bold"))
+split_label1.grid(row=0, column=0, pady=5, sticky='n')
+
+split_label2 = tk.Label(middle_frame, text="--------------干音分析设定--------------", font=("Helvetica", 10, "bold"))
+split_label2.grid(row=11, column=0, pady=5, sticky='n')
+
+split_label2 = tk.Label(middle_frame, text="-------------处理进度与结果-------------", font=("Helvetica", 10, "bold"))
+split_label2.grid(row=14, column=0, pady=5, sticky='n')
+#app.drop_target_register(DND_FILES)
+#app.dnd_bind('<<Drop>>', on_drop)
+
 app.mainloop()
